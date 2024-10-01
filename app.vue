8654c55b831b810b9b4ebd1f70cb91a7496def77
matheoftne <matheo.fontaine.pro@gmail.com>
2024-10-01 21:02:22 +0200

    first commit

---
+++ app.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup>
+import Main from './src/pages/Main.vue'
+</script>
+
+<template>
+  <Main />
+</template>
---
+++ src/pages/Main.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Weather Tracker</h1>
+  </div>
+</template>
+
+<script lang="ts">
+const { data: weatherData } = await useFetch('/api/weather')
+console.log(data)
+</script>
+
+<style>
+
+</style>
